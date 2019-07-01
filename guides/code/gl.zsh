3c49a64c3022ff0fad544fcdd5345b3ccb7044d6
thoga31 <thoga31@gmail.com>
2019-07-01 03:36:26 +0100

    Fedora 30 — OpenGL
    Install GLFW and automate things with shell scripts

---
+++ guides/code/gl.zsh
@@ -0,0 +1,5 @@
+rungl() {
+    FILENAME=${$(basename -- "$@")%.*}
+    g++ -std=c++11 -c "$FILENAME.cpp"
+    g++ "$FILENAME.o" -o "$FILENAME" -lGL -lGLU -lglfw3 -lX11 -lXxf86vm -lXrandr -lpthread -lXi -ldl -lXinerama -lXcursor
+}
